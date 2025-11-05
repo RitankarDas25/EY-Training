2a889ba97a81629bda59109b2cbae5afaafbe6e7
RitankarDas25 <ritankar25@gmail.com>
2025-11-05 14:11:12 +0530

    4th commit

---
+++ 5th_Nov/project/backend/routes/summarize_route.py
@@ -0,0 +1,21 @@
+from fastapi import APIRouter, UploadFile
+import tempfile
+import time
+from backend.utils.pdf_utils import load_pdf, chunk_documents
+from backend.utils.llm_utils import get_llm
+from backend.services.summarize_service import map_reduce_summary
+
+router = APIRouter(prefix="/summarize", tags=["Summarization"])
+
+@router.post("/")
+async def summarize_pdf(file: UploadFile):
+    with tempfile.NamedTemporaryFile(delete=False, suffix=".pdf") as tmp:
+        tmp.write(await file.read())
+        pdf_path = tmp.name
+
+    docs = load_pdf(pdf_path)
+    chunks = chunk_documents(docs)
+    llm = get_llm()
+    summary = map_reduce_summary(chunks, llm)
+
+    return {"summary": summary}
